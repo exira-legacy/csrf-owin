e873da574d716afd971c96420faf090ecca73e6e
David Cumps <david@cumps.be>
2015-12-29 22:27:08 +0100

    Target 4.6.1

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+#### 0.3 - December 29 2015
+* NuGet package is now targeting .NET 4.6.1
+
 #### 0.2 - December 6 2015
 * Fix ExcludedPaths
 
--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Exira.CsrfValidation.Owin")>]
 [<assembly: AssemblyProductAttribute("Exira.CsrfValidation.Owin")>]
 [<assembly: AssemblyDescriptionAttribute("Exira.CsrfValidation.Owin is an OWIN middleware to prevent CSRF attacks by validating HTTP Headers and Cookies.")>]
-[<assembly: AssemblyVersionAttribute("0.2.7")>]
-[<assembly: AssemblyFileVersionAttribute("0.2.7")>]
-[<assembly: AssemblyMetadataAttribute("githash","2e67b31034377bcbd1bdef56ee593ebd67210feb")>]
+[<assembly: AssemblyVersionAttribute("0.3.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.0")>]
+[<assembly: AssemblyMetadataAttribute("githash","18e18d52adde7df075d016477db7eb949b34e129")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.2.7"
+    let [<Literal>] Version = "0.3.0"
