fa1e32701570c1dd64b0f66d793a78387cf30a84
Team City <info@exira.com>
2015-12-29 21:31:30 +0000

    Bump version to 0.3.8

--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Exira.CsrfValidation.Owin")>]
 [<assembly: AssemblyProductAttribute("Exira.CsrfValidation.Owin")>]
 [<assembly: AssemblyDescriptionAttribute("Exira.CsrfValidation.Owin is an OWIN middleware to prevent CSRF attacks by validating HTTP Headers and Cookies.")>]
-[<assembly: AssemblyVersionAttribute("0.3.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.3.0")>]
-[<assembly: AssemblyMetadataAttribute("githash","18e18d52adde7df075d016477db7eb949b34e129")>]
+[<assembly: AssemblyVersionAttribute("0.3.8")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.8")>]
+[<assembly: AssemblyMetadataAttribute("githash","e873da574d716afd971c96420faf090ecca73e6e")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.3.0"
+    let [<Literal>] Version = "0.3.8"
